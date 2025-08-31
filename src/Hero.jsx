187daa1301c6a4774068e745fce7a3a9dea38642
Swayam <swayamkumar226@gmail.com>
2025-08-31 11:59:05 +0530

    list working

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,27 +1,38 @@
+import { useState } from "react";
 export default function Hero() {
 
-    const ingredients = ["chicken", "spinach", "tomatoes"]
+    const [ingredients, setIngredients] = useState([]);
 
     const items = ingredients.map(element=>(
         <li key={element}>{element}</li>
     ))
 
-    function handleSubmit() {
-        console.log("submitted")
+    function handleSubmit(event) {
+        event.preventDefault();
+        const formdata = new FormData(event.currentTarget)
+        const newitem = formdata.get("ingredient")
+        setIngredients((prevstate) => [...prevstate, newitem])
     }
 
     return (
-        <main className="flex justify-center pt-20 flex-col gap-20">
-            <div id="tagline" className="flex justify-center">
-                <h1 className="text-7xl text-amber-900">Your urge deserves better</h1>
+        <main className="flex justify-center items-center pt-20 flex-col gap-20 mr-35 ml-35">
+            <div id="tagline" className="flex justify-center w-full max-w-4xl px-4">
+                <div className="text-center">
+                    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-amber-900 leading-tight text-shadow-amber">Few ingredients in the kitchen?
+                    </h1>
+                    <p className="text-2xl font-semibold md:text-3xl text-amber-800 mt-2">
+                        No Worries, we've got you covered!
+                    </p>
+                </div>
             </div>
             <form className="flex flex-row gap-15 justify-center" onSubmit={handleSubmit}>
                 <input
                 aria-label="Add Ingredients" type="text"
                 placeholder="e.g. dhaniya"
-                className="shadow-md bg-gray-100 h-12 w-150 placeholder: pl-2"
+                className="shadow-md bg-gray-100 h-12 w-150 rounded-md placeholder: pl-2"
+                name="ingredient"
                 />
-                <button type="submit" className="rounded-sm bg-amber-400  h-12 w-75 shadow-md placeholder: text-[18px]">+ Add Ingredient</button>
+                <button type="submit" className="rounded-md bg-amber-400  h-12 w-75 shadow-md placeholder: text-[18px]">+ Add Ingredient</button>
             </form>
 
             <ul className="list-disc list-inside">
