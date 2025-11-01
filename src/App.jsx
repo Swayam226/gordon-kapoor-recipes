d7fba1496c21f9228d30ab9923e68da08959bf28
Swayam <swayamkumar226@gmail.com>
2025-11-01 22:17:46 +0530

    analytics bug fixed again

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <>
-      <div className="min-h-screen w-full bg-white relative overflow-hidden">
-        <div
-          className="absolute inset-0 z-0"
-          style={{
-            backgroundImage: `
+    <div className="min-h-screen w-full bg-white relative overflow-hidden">
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `
             radial-gradient(135% 135% at 50% 90%, #ffffff 30%, #f59e0b 100%)
           `,
-            backgroundSize: "100% 100%",
-          }}
-        />
-        <div className="relative z-10">
-          {/* <Header
+          backgroundSize: "100% 100%",
+        }}
+      />
+      <div className="relative z-10">
+        {/* <Header
           src="/chef-claude-icon.png"
           alt="icon of site"
           imgclass="size-12"
         /> */}
-          <Hero />
-        </div>
+        <Hero />
       </div>
       <Analytics />
-    </>
+    </div>
   );
 }
 
